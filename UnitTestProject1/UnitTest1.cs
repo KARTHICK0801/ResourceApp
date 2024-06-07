245cc084049f547d9245d7d5a87a41cbd9ef528c
KARTHICK S <sudhakarkutty20@gmail.com>
2024-06-07 19:24:30 +0530

    07.24pm

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -13,40 +13,76 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            ResourceD Data = new ResourceD();
+            ResourceM Mod = new ResourceM();
+            ResourceR Rep = new ResourceR();
+            AdditionalJson adJson = new AdditionalJson();
             try
             {
-                ResourceD Data = new ResourceD();
-                ResourceM Mod = new ResourceM();
-                ResourceR Rep = new ResourceR();
-                AdditionalJson adJson = new AdditionalJson();
+                Console.WriteLine("Insert--1,Update--2");
+                int input = int.Parse(Console.ReadLine());
+                switch (input) {
+                    case 1:
+                    Mod.ResourceType = "Developer";
+                    Mod.ResourceCode = "DB9090";
+                    Mod.ResourceName = "Kavi";
+                    Mod.PrimaryPhone = "8907654321";
+                    Mod.EmailID = "ak@gmail.com";
+                    Mod.IsActive = true;
+                    adJson.Address1 = "09";
+                    adJson.Address2 = "North-Usman";
+                    adJson.Address3 = "Chennai";
+                    adJson.Address4 = 600028;
+                    adJson.DOB = "09/09/1999";
+                    adJson.Gender = "Male";
+                    Mod.AdditionalInfo = adJson;
 
-                Mod.ResourceType = "Developer";
-                Mod.ResourceCode = "DB9090";
-                Mod.ResourceName = "Kavi";
-                Mod.PrimaryPhone = "8907654321";
-                Mod.EmailID = "ak@gmail.com";
-                Mod.IsActive = true;
-                adJson.Address1 = "09";
-                adJson.Address2 = "North-Usman";
-                adJson.Address3 = "Chennai";
-                adJson.Address4 = 600028;
-                adJson.DOB = "09/09/1999";
-                adJson.Gender = "Male";
-                Mod.AdditionalInfo = adJson;
+                    Data.InsertResources(Mod);
 
-                Data.InsertResources(Mod);
+                    var responce = new
+                    {
+                        Responce_Code = 2000,
+                        ResponseMessage = "Success",
+                        ResponseList = JsonConvert.SerializeObject(Mod)
+                    };
 
-                var responce = new
-                {
-                    Responce_Code = 2000,
-                    ResponseMessage = "Success",
-                    ResponseList =JsonConvert.SerializeObject(Mod)
-            };
+                    string s = JsonConvert.SerializeObject(responce);
 
-                string s = JsonConvert.SerializeObject(responce);
+                    Console.WriteLine(s);
+                    break;
+                    case 2:
+                        Mod.ResourceUID = 2;
+                        Mod.ResourceType = "Developer";
+                        Mod.ResourceCode = "DB9090";
+                        Mod.ResourceName = "pavi";
+                        Mod.PrimaryPhone = "8907654321";
+                        Mod.EmailID = "ak@gmail.com";
+                        Mod.IsActive = true;
+                        adJson.Address1 = "09";
+                        adJson.Address2 = "North-Usman";
+                        adJson.Address3 = "Chennai";
+                        adJson.Address4 = 600028;
+                        adJson.DOB = "09/09/1999";
+                        adJson.Gender = "Male";
+                        Mod.AdditionalInfo = adJson;
 
-                Console.WriteLine(s);
+                        Data.UpdateResource(Mod);
 
+                        var responce1 = new
+                        {
+                            Responce_Code = 2000,
+                            ResponseMessage = "Success",
+                            ResponseList = JsonConvert.SerializeObject(Mod)
+                        };
+
+                        string s1 = JsonConvert.SerializeObject(responce1);
+
+                        Console.WriteLine(s1);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
                 Console.ReadKey();
             }
             catch (Exception ex)
